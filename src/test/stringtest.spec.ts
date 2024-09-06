169f47aa00546b36eee9f2af9e84b78e127c2c26
asifsa12 <asfalli.asif@gmail.com>
2024-09-06 01:33:52 -0400

    Initial commit without nested repository

---
+++ src/test/stringtest.spec.ts
@@ -0,0 +1,8 @@
+//declare string data type and declare as message 
+let string = ("Welcome to Java class");
+//declare int type and name it 
+let number = 101;
+//print both variables in one print statement
+console.log ("My values are" + string + number)
+//test comment 3 for playwright
+//
